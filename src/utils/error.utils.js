62a635a0e8b9828a7114af0ce9b85402cc0169cd
h5avlov <h5avlov@abv.bg>
2025-08-07 15:06:25 +0300

    Util to uniform errors added

---
+++ src/utils/error.utils.js
@@ -0,0 +1,23 @@
+const { Result } = require("express-validator");
+const ValidationError = require("../errors/ValidationError");
+
+module.exports = {
+    normalize: (error) => { 
+        let errors = {}; 
+
+        if (error instanceof Result) { 
+            error.errors.map(e => {
+                errors[e.path] = { 
+                    message: e.msg 
+                }; 
+            }); 
+        }
+        
+        if (error instanceof ValidationError) { 
+            errors[error.path] = { message: error.message }; 
+        }
+
+        return errors; 
+
+    }
+}; 
