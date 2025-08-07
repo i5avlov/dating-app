546eb2831b4c5bd188e2dfa85c840f70b5cff788
h5avlov <h5avlov@abv.bg>
2025-08-07 16:02:49 +0300

    Removed validations from User model

--- src/models/User.js
+++ src/models/User.js
@@ -1,18 +1,9 @@
 const { Schema, model } = require('mongoose'); 
 
 const userSchema = new Schema({
-    username: {
-        type: String, 
-        required: true 
-    }, 
-    email: {
-        type: String, 
-        required: true 
-    }, 
-    password: {
-        type: String, 
-        required: true 
-    }
+    username: String, 
+    email: String, 
+    password: String 
 }); 
 
 const User = model('User', userSchema); 
