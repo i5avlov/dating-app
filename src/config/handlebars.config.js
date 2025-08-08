215f62e405a05068d57cb96ea2536018df123afb
h5avlov <h5avlov@abv.bg>
2025-08-08 21:57:46 +0300

    Select helper registered

--- src/config/handlebars.config.js
+++ src/config/handlebars.config.js
@@ -1,8 +1,12 @@
 const handlebars = require('express-handlebars'); 
+const helpersHandlebars = require('../helpers/helpers.handlebars');
 
 module.exports = {
     config: (app) => { 
-        app.engine('hbs', handlebars.engine({ extname: 'hbs' })); 
+        app.engine('hbs', handlebars.engine({ 
+            extname: 'hbs', 
+            helpers: helpersHandlebars 
+        })); 
         app.set('view engine', 'hbs'); 
         app.set('views', 'src/views'); 
 
