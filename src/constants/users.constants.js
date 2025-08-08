627195d91f47a819325b886c020073fdf3f93f4d
h5avlov <h5avlov@abv.bg>
2025-08-08 22:06:10 +0300

    Default user on page count changed to 5

--- src/constants/users.constants.js
+++ src/constants/users.constants.js
@@ -1,7 +1,7 @@
 module.exports = { 
     PAGINATION: { 
         PAGE_NUMBER: 1, 
-        USERS_PER_PAGE_COUNT: 3, 
+        USERS_PER_PAGE_COUNT: 5, 
         USERS_PER_PAGE_OPTIONS: [ 2, 3, 5, 10 ] 
     }
 
