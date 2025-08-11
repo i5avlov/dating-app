396c2b9e79240c4b2384f42567576d20bec081ef
h5avlov <h5avlov@abv.bg>
2025-08-11 06:49:42 +0300

    Handlebars helpers to separate sent and received messages in view

--- src/helpers/helpers.handlebars.js
+++ src/helpers/helpers.handlebars.js
@@ -3,27 +3,12 @@ module.exports = {
         return currentOption === targetOption ? mark : ''; 
     }, 
 
-    slice: (text = '', charCount = 0) => { 
-        return text.slice(0, charCount) + '...'; 
-    }, 
+    areMessagesSent: (messagesToGet) => { 
+        return messagesToGet === 'sent'; 
+    },  
 
-    getOtherUserId: (messagesGroup, senderId, receiverId) => { 
-        if (messagesGroup === 'sent') { 
-            return receiverId; 
-        } 
-
-        if (messagesGroup === 'received') { 
-            return senderId; 
-        } 
-
-    }, 
-
-    areMessagesSent: (messagesGroup = '') => { 
-        return messagesGroup === 'sent'; 
-    }, 
-
-    areMessagesReceived: (messagesGroup = '') => { 
-        return messagesGroup === 'received'; 
+    areMessagesReceived: (messagesToGet) => { 
+        return messagesToGet === 'received'; 
     }
 
 }; 
