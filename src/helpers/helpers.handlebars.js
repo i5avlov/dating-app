98fa0680741c43db831568faaea52c1543f85722
h5avlov <h5avlov@abv.bg>
2025-08-11 06:10:55 +0300

    Handlebars helper to slice a part of the text message in index view added

--- src/helpers/helpers.handlebars.js
+++ src/helpers/helpers.handlebars.js
@@ -1,6 +1,29 @@
 module.exports = { 
     markOnEqual: (currentOption, targetOption, mark) => { 
         return currentOption === targetOption ? mark : ''; 
+    }, 
+
+    slice: (text = '', charCount = 0) => { 
+        return text.slice(0, charCount) + '...'; 
+    }, 
+
+    getOtherUserId: (messagesGroup, senderId, receiverId) => { 
+        if (messagesGroup === 'sent') { 
+            return receiverId; 
+        } 
+
+        if (messagesGroup === 'received') { 
+            return senderId; 
+        } 
+
+    }, 
+
+    areMessagesSent: (messagesGroup = '') => { 
+        return messagesGroup === 'sent'; 
+    }, 
+
+    areMessagesReceived: (messagesGroup = '') => { 
+        return messagesGroup === 'received'; 
     }
 
 }; 
